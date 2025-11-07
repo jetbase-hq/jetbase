e41f1071d7f5720e4f268e79e467d4951b91b12c
jaz-alli <jaz.allibhai@gmail.com>
2025-11-06 22:53:49 -0500

    fixed intialize func typo (#12)

--- jetbase/core/initialize.py
+++ jetbase/core/initialize.py
@@ -29,5 +29,5 @@ def create_directory_structure(base_path: str) -> None:
     print(f"Initialized Jetbase project in {Path(base_path).absolute()}")
 
 
-def initilize_cmd() -> None:
+def initialize_cmd() -> None:
     create_directory_structure(base_path=BASE_DIR)
