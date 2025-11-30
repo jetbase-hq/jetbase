522616e27919117d0cee24a9c40770b72bb9aa54
jaz-alli <jaz.allibhai@gmail.com>
2025-11-30 00:59:54 -0500

    added checksum checks (#27)
    * added checksum checks
    
    * lint
    
    * remove auto publish on release

--- jetbase/core/upgrade.py
+++ jetbase/core/upgrade.py
@@ -1,11 +1,19 @@
 import os
 
+from jetbase.core.checksum import (
+    validate_current_migration_files_match_checksums,
+    validate_migrated_versions_in_current_migration_files,
+    validate_no_duplicate_migration_file_versions,
+    validate_no_new_migration_files_with_lower_version_than_latest_migration,
+)
 from jetbase.core.dry_run import process_dry_run
 from jetbase.core.file_parser import parse_upgrade_statements
 from jetbase.core.lock import create_lock_table_if_not_exists, migration_lock
 from jetbase.core.repository import (
     create_migrations_table_if_not_exists,
+    get_checksums_by_version,
     get_last_updated_version,
+    get_migrated_versions,
     run_migration,
 )
 from jetbase.core.version import get_migration_filepaths_by_version
@@ -33,14 +41,17 @@ def upgrade_cmd(
     create_migrations_table_if_not_exists()
     create_lock_table_if_not_exists()
 
-    latest_version: str | None = get_last_updated_version()
+    latest_migrated_version: str | None = get_last_updated_version()
+
+    if latest_migrated_version:
+        run_upgrade_validations(latest_migrated_version=latest_migrated_version)
 
     all_versions: dict[str, str] = get_migration_filepaths_by_version(
         directory=os.path.join(os.getcwd(), "migrations"),
-        version_to_start_from=latest_version,
+        version_to_start_from=latest_migrated_version,
     )
 
-    if latest_version:
+    if latest_migrated_version:
         all_versions = dict(list(all_versions.items())[1:])
 
     if count:
@@ -79,3 +90,40 @@ def upgrade_cmd(
             version_to_filepath=all_versions,
             migration_operation=MigrationOperationType.UPGRADE,
         )
+
+
+def run_upgrade_validations(latest_migrated_version: str) -> None:
+    """
+    Run validations on migration files before performing upgrade.
+    """
+
+    migrations_directory: str = os.path.join(os.getcwd(), "migrations")
+
+    migration_filepaths_by_version: dict[str, str] = get_migration_filepaths_by_version(
+        directory=migrations_directory
+    )
+    validate_no_duplicate_migration_file_versions(
+        current_migration_filepaths_by_version=migration_filepaths_by_version
+    )
+
+    migrated_versions: list[str] = get_migrated_versions()
+
+    validate_migrated_versions_in_current_migration_files(
+        migrated_versions=migrated_versions,
+        current_migration_filepaths_by_version=migration_filepaths_by_version,
+    )
+
+    validate_no_new_migration_files_with_lower_version_than_latest_migration(
+        current_migration_filepaths_by_version=migration_filepaths_by_version,
+        migrated_versions=migrated_versions,
+        latest_migrated_version=latest_migrated_version,
+    )
+
+    migrated_filepaths_by_version: dict[str, str] = get_migration_filepaths_by_version(
+        directory=migrations_directory, end_version=latest_migrated_version
+    )
+
+    validate_current_migration_files_match_checksums(
+        migrated_filepaths_by_version=migrated_filepaths_by_version,
+        migrated_versions_and_checksums=get_checksums_by_version(),
+    )
